072fa71fbca931eaab355c824166cf5abce9b31b
Scott Bergler <commandinghands@gmail.com>
2020-02-11 18:34:21 -0800

    Enable strict: true.

--- scripts.ts
+++ scripts.ts
@@ -1,11 +1,16 @@
-let root: HTMLElement = document.documentElement;
+let root: HTMLElement | null = document.documentElement;
 
-let button: HTMLButtonElement = document.querySelector('button');
-button.addEventListener('click', handleClick);
+let button: HTMLButtonElement | null = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', handleClick);
+}
 
 function handleClick(): void {
   let color: string = '#000000'.replace(/0/g, function() {
     return Math.floor(Math.random() * 16).toString(16);
   });
-  root.style.setProperty('--bg-color', color);
+  if (root) {
+    root.style.setProperty('--bg-color', color);
+  }
 }
