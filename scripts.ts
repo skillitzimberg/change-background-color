6896640c1967a5de058a9f77dc17754ec5d55945
Scott Bergler <commandinghands@gmail.com>
2020-02-11 17:40:14 -0800

    Add types to scripts.ts.

--- scripts.ts
+++ scripts.ts
@@ -1,10 +1,10 @@
-let root = document.documentElement;
+let root: HTMLElement = document.documentElement;
 
-let button = document.querySelector('button');
+let button: HTMLButtonElement = document.querySelector('button');
 button.addEventListener('click', handleClick);
 
-function handleClick() {
-  let color = '#000000'.replace(/0/g, function() {
+function handleClick(): void {
+  let color: string = '#000000'.replace(/0/g, function() {
     return Math.floor(Math.random() * 16).toString(16);
   });
   root.style.setProperty('--bg-color', color);
