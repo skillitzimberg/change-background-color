3d9ee13802a5288193b7673c7b53952978f26706
Scott Bergler <commandinghands@gmail.com>
2020-02-12 12:45:55 -0800

    Update README.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 var root = document.documentElement;
 var button = document.querySelector('button');
 if (button) {
-    button.addEventListener('click', handleClick);
+  button.addEventListener('click', handleClick);
 }
 function handleClick() {
-    var color = '#000000'.replace(/0/g, function () {
-        return Math.floor(Math.random() * 16).toString(16);
-    });
-    if (root) {
-        root.style.setProperty('--bg-color', color);
-    }
+  var color = '#000000'.replace(/0/g, function() {
+    return Math.floor(Math.random() * 16).toString(16);
+  });
+  if (root) {
+    root.style.setProperty('--bg-color', color);
+  }
 }
