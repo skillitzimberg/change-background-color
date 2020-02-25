28fba2d8edce26915f9e785524c046630ac25605
Scott Bergler <commandinghands@gmail.com>
2020-02-25 06:52:48 -0800

    Add a test for background color changes.

--- scripts.test.ts
+++ scripts.test.ts
@@ -1,7 +1,18 @@
-const sum = require('./scripts.js');
+import { sum, root, handleClick } from './scripts';
 
 describe('Functions do math', () => {
-  document.body.innerHTML = test('it adds', () => {
+  test('it adds', () => {
     expect(sum(2, 3)).toEqual(5);
+  });
+});
+
+describe('DOM elements exist', () => {
+  test('handleClick changes root background color', () => {
+    if (root) console.log(root.style.getPropertyValue('--bg-color'));
+    const button = document.createElement('button');
+    button.addEventListener('click', handleClick);
+    button.click();
+    button.click();
+    if (root) expect(root.style.backgroundColor === '#495935').toBeFalsy();
   });
 });
